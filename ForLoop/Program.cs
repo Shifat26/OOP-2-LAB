aad2d600a715589d9e999f39f3ddd947c59fc55d
Shifat <mahabub.shifat@gmail.com>
2021-02-08 10:14:19 +0600

    Shifat

---
+++ ForLoop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n;
+            n = 10;
+            for(i=1;i<n;i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i+"");
+                }
+            }
+        }
+    }
+}
